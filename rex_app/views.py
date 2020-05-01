2975b178f338512dc80c4b8762ad8e04c23a37e0
Rex <rexchao27@gmail.com>
2020-05-01 19:07:35 +0800

    templating

--- rex_app/views.py
+++ rex_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from random import randint
+
 # Create your views here.
 def index(request):
-	return HttpResponse('Welcome!')
+	# return HttpResponse('<b>Welcome!</b>')
+	return render(request, 'rex_app/b.html', {
+		'sunny_today': True,
+		'a_big_number': 23094820394803948309,
+		'fruits': ['apple','banana','orange'],
+		'd': {'a': 1, 'b': 2},
+		})
