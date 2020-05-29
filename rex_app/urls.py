148b11e36f22a105f1050bef10c0bbafc2421355
dw <white.dh@gmail.com>
2020-05-29 19:04:36 +0800

    forms!

--- rex_app/urls.py
+++ rex_app/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
 
     path('index', views.index, name='index'),
     path('inc_age', views.inc_age, name='inc_age'),
+    path('question/create', views.create_question, name='create_question'),
+    path('answer/create/<int:question_pk>', views.create_answer, name='create_answer'),
     path('question/<int:pk>', views.question_detail, name='question_detail'),
+
     
     
 ]
