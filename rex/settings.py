34cec22f0fffa66ebeafd2a462015cb64feb5d93
Rex Chao <rexchao27@gmail.com>
2020-09-11 17:56:44 +0800

    whitenoise

--- rex/settings.py
+++ rex/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'rex.urls'
@@ -132,8 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-print('hello django')
-
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # LOGGING = {
 #     'version': 1,
 #     'disable_existing_loggers': False,
